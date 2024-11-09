3d3921ab0a5b9ab3f5850a8fadcfd7555a197cff
Axel Rivera <axel@axelrivera.me>
2024-11-09 16:14:25 -0500

    Initial Commit

---
+++ HealthImportDemo/HealthImportDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthImportDemoApp.swift
+//  HealthImportDemo
+//
+//  Created by Axel Rivera on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthImportDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
